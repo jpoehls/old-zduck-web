ce3f784c1abbddae9986a64c9622ab110bfc6a70
Joshua Poehls <joshua@zduck.com>
2009-10-01 01:22:58 -0500

    added initial offsite_redircts.py script to handle redirecting to my flickr guest pass urls

---
+++ offsite_redirects.py
@@ -0,0 +1,31 @@
+from google.appengine.ext import webapp
+from google.appengine.ext.webapp.util import run_wsgi_app
+
+class Photos(webapp.RequestHandler):
+  def get(self):
+    if "fishnpaws.com" in self.request.url:
+      # Mom & Aunt Lorell's FRIENDS photostream
+      self.redirect("http://www.flickr.com/gp/26412493@N07/7R425u")
+    else:
+      # Joshua & Kristina's FRIENDS photostream
+      self.redirect("http://www.flickr.com/gp/25531422@N08/4p6S43")
+
+class FamilyPhotos(webapp.RequestHandler):
+  def get(self):
+    if "fishnpaws.com" in self.request.url:
+      # Mom & Aunt Lorell's FAMILY & FRIENDS photostream
+      self.redirect("http://www.flickr.com/gp/26412493@N07/u86x6e")
+    else:
+      # Joshua & Kristina's FAMILY & FRIENDS photostream
+      self.redirect("http://www.flickr.com/gp/25531422@N08/2AZM06")
+
+application = webapp.WSGIApplication(
+                                     [('/photos', Photos),
+                                      ('/familyphotos', FamilyPhotos)],
+                                     debug=True)
+
+def main():
+  run_wsgi_app(application)
+
+if __name__ == "__main__":
+  main()
